3244a0af3f5d2e2d9ef0be8a3d3002428e3ce02c
Petr Kessler <kesspess@gmail.com>
2013-02-11 13:38:29 +0100

    helpers separated

--- app/ExamplePresenter.php
+++ app/ExamplePresenter.php
@@ -11,31 +11,8 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 	/** @var Nette\Database\Connection */
 	protected $ndb;
 
-	/** @var array */
-	protected $countries = array(
-		'au' => 'Australia',
-		'at' => 'Austria',
-		'be' => 'Belgium',
-		'ca' => 'Canada',
-		'ch' => 'Switzerland',
-		'cz' => 'Czech Republic',
-		'de' => 'Germany',
-		'es' => 'Spain',
-		'fi' => 'Finland',
-		'fr' => 'France',
-		'gb' => 'United Kingdom',
-		'hu' => 'Hungary',
-		'is' => 'Iceland',
-		'it' => 'Italy',
-		'pl' => 'Poland',
-		'se' => 'Sweden',
-		'us' => 'United States',
-	);
-
 	/** @var Nette\Caching\Cache */
 	protected $cache;
-
-	const SCRIPT_KEY = 'grid-script-';
 
 
 
@@ -43,7 +20,6 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 
 	protected function createComponentDataGrid()
 	{
-		$me = $this;
 		$grid = $this->context->createDataGrid();
 		$grid->setTemplateFile( __DIR__ . '/user-grid.latte' );
 
@@ -79,24 +55,22 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 	{
 		$container = new Nette\Forms\Container;
 
-		/* $container->addSelect('gender', 'Pohlaví', array(
-			'male' => 'Muž',
-			'female' => 'Žena',
-		))->setPrompt('---'); */
-
 		$container->addText('firstname');
 		$container->addText('surname');
 
 		$birthday = $container->addContainer('birthday');
-		$min = $this->addDateInput( $birthday, 'min' );
-		$max = $this->addDateInput( $birthday, 'max' );
+		$min = Helpers::addDateInput( $birthday, 'min' );
+		$max = Helpers::addDateInput( $birthday, 'max' );
 
-		$parser = $this->parseDate;
+		$parser = callback('Helpers::parseDate');
 		$min->addCondition( Form::FILLED )->addRule( function () use ($min, $max, $parser) {
-			return !$max->filled || (($minDt = $parser( $min->value )) !== FALSE && ($maxDt = $parser( $max->value )) !== FALSE && $minDt <= $maxDt);
+			return !$max->filled
+					|| (($minDt = $parser( $min->value )) !== FALSE
+						&& ($maxDt = $parser( $max->value )) !== FALSE
+						&& $minDt <= $maxDt);
 		}, 'Minimální datum nesmí následovat po maximálním.' );
 
-		$container->addSelect( 'country_code', 'Země', $this->countries )
+		$container->addSelect( 'country_code', 'Země', Helpers::getCountries() )
 				->setPrompt('---');
 
 		$container->addText('kilograms')->addCondition( Form::FILLED )->addRule( Form::FLOAT );
@@ -111,9 +85,9 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 		$container = new Nette\Forms\Container;
 		$container->addText('firstname')->setRequired('Zadejte prosím jméno.');
 		$container->addText('surname')->setRequired('Zadejte prosím příjmení.');
-		$container->addSelect( 'country_code', 'Země', $this->countries )->setRequired('Zvolte zemi původu.')
+		$container->addSelect( 'country_code', 'Země', Helpers::getCountries() )->setRequired('Zvolte zemi původu.')
 				->setDefaultValue( $record->country_code );
-		$this->addDateInput($container, 'birthday')->setRequired('Zadejte datum narození.');
+		Helpers::addDateInput($container, 'birthday')->setRequired('Zadejte datum narození.');
 		$container->addText('kilograms')->addRule( Form::FLOAT, 'Váhu zadejte jako číslo.' );
 		$defaults = $record->toArray();
 		$defaults['birthday'] = id( new DateTime($defaults['birthday']) )->format('d. m. Y');
@@ -145,8 +119,8 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 				$conds[$column] = $value;
 
 			} elseif ($column === 'birthday') {
-				isset($value['min']) && $conds["$column >= ?"] = $this->parseDate( $value['min'] )->format('Y-m-d');
-				isset($value['max']) && $conds["$column <= ?"] = $this->parseDate( $value['max'] )->format('Y-m-d');
+				isset($value['min']) && $conds["$column >= ?"] = Helpers::parseDate( $value['min'] )->format('Y-m-d');
+				isset($value['max']) && $conds["$column <= ?"] = Helpers::parseDate( $value['max'] )->format('Y-m-d');
 
 			} elseif ($column === 'kilograms') {
 				$conds["$column <= ?"] = $value;
@@ -193,7 +167,7 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 
 
 
-	// === HELPERS & APP-RELATED STUFF ===============================================================
+	// === APP-RELATED STUFF & HELPERS ===============================================================
 
 	function inject(Nette\Database\Connection $c, Nette\Caching\IStorage $s)
 	{
@@ -208,30 +182,7 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 		parent::loadState($params);
 
 		if ($this->showQueries) {
-			$me = $this;
-			$this->payload->queries = array();
-			$this->ndb->onQuery[] = function ($s) use ($me) { $me->logQuery( $s->queryString ); };
-		}
-	}
-
-
-
-	function logQuery($sql)
-	{
-		$this->payload->queries[] = dibi::dump( $sql, TRUE );
-	}
-
-
-
-	protected function loadClientScripts()
-	{
-		foreach (array('js/twigrid.datagrid.js', 'css/twigrid.datagrid.css') as $file) {
-			( ( $key = static::SCRIPT_KEY . $file ) && is_file( $dest = __DIR__ . '/../' . $file ) && $this->cache->load( $key ) ) || (
-				copy($source = __DIR__ . '/../libs/TwiGrid/client-side/' . basename($file), $dest)
-				&& $this->cache->save($key, TRUE, array(
-					Nette\Caching\Cache::FILES => array($source),
-				))
-			);
+			Helpers::initQueryLogging($this->ndb, $this->payload);
 		}
 	}
 
@@ -239,41 +190,12 @@ class ExamplePresenter extends Nette\Application\UI\Presenter
 
 	protected function createTemplate($class = NULL)
 	{
-		$this->loadClientScripts();
+		Helpers::loadClientScripts($this->cache, __DIR__ . '/..');
 		$this->invalidateControl('links');
 		$this->invalidateControl('flashes');
 		id ($template = parent::createTemplate($class))->showQueries = $this->showQueries;
 		return $template
 				->registerHelper('mtime', function ($f) { return $f . '?' . filemtime( __DIR__ . '/../' . $f ); })
 				->setFile( __DIR__ . "/views/{$this->view}.latte" );
-	}
-
-
-
-	protected function addDateInput(Nette\Forms\Container $container, $name)
-	{
-		$control = $container->addText($name);
-		$parser = $this->parseDate;
-		$control->addCondition( Form::FILLED )->addRule( function ($control) use ($parser) {
-			return $parser( $control->value ) !== FALSE;
-		}, 'Datum prosím zadávejte ve formátu "D.M.RRRR".' );
-
-		return $control;
-	}
-
-
-
-	function parseDate($s)
-	{
-		try {
-			if (!($m = Nette\Utils\Strings::match($s, '#^\s*(0[1-9]|[12][0-9]|3[01])\s*\.\s*(0?[1-9]|1[0-2])\s*\.\s*([0-9]{4})\s*$#'))) {
-				return FALSE;
-			}
-
-			return new DateTime("{$m[3]}-{$m[2]}-{$m[1]}");
-
-		} catch (Exception $e) {}
-
-		return FALSE;
 	}
 }
---
+++ app/Helpers.php
@@ -0,0 +1,106 @@
+<?php
+
+use Nette\Forms\Form;
+use Nette\Caching\Cache;
+use Nette\Forms\Container;
+use Nette\Database\Statement;
+use Nette\Database\Connection;
+
+
+/**
+ * Misc helpers for demo purposes
+ */
+class Helpers
+{
+
+	const SCRIPT_KEY = 'grid-script-';
+	const DATE_REGEXP = '#^\s*(0[1-9]|[12][0-9]|3[01])\s*\.\s*(0?[1-9]|1[0-2])\s*\.\s*([0-9]{4})\s*$#';
+
+
+
+	static function getCountries()
+	{
+		return array(
+			'au' => 'Australia',
+			'at' => 'Austria',
+			'be' => 'Belgium',
+			'ca' => 'Canada',
+			'ch' => 'Switzerland',
+			'cz' => 'Czech Republic',
+			'de' => 'Germany',
+			'es' => 'Spain',
+			'fi' => 'Finland',
+			'fr' => 'France',
+			'gb' => 'United Kingdom',
+			'hu' => 'Hungary',
+			'is' => 'Iceland',
+			'it' => 'Italy',
+			'pl' => 'Poland',
+			'se' => 'Sweden',
+			'us' => 'United States',
+		);
+	}
+
+
+
+	static function initQueryLogging(Connection $connection, $payload)
+	{
+		$logger = callback(__CLASS__, 'logQuery');
+		$payload->queries = array();
+		$connection->onQuery[] = function (Statement $st) use ($logger, $payload) {
+			$logger($payload, $st->queryString);
+		};
+	}
+
+
+
+	static function logQuery($payload, $sql)
+	{
+		$payload->queries[] = dibi::dump($sql, TRUE);
+	}
+
+
+
+	static function loadClientScripts(Cache $cache, $baseDir)
+	{
+		foreach (array('js/twigrid.datagrid.js', 'css/twigrid.datagrid.css') as $file) {
+			( ( $key = static::SCRIPT_KEY . $file ) && is_file( $dest = $baseDir . '/' . $file )
+					&& $cache->load( $key ) ) || (
+				copy($source = $baseDir . '/libs/TwiGrid/client-side/' . basename($file), $dest)
+					&& $this->cache->save($key, TRUE, array(
+						Cache::FILES => array($source),
+					))
+			);
+		}
+	}
+
+
+
+	static function addDateInput(Container $container, $name)
+	{
+		$control = $container->addText($name);
+		$parser = callback(__CLASS__, 'parseDate');
+		$control->addCondition( Form::FILLED )->addRule( function ($control) use ($parser) {
+			return $parser($control->value) !== FALSE;
+		}, 'Datum prosím zadávejte ve formátu "D.M.RRRR".' );
+
+		return $control;
+	}
+
+
+
+	static function parseDate($s)
+	{
+		try {
+			if (!($m = Nette\Utils\Strings::match($s, static::DATE_REGEXP))) {
+				return FALSE;
+			}
+
+			return new DateTime("{$m[3]}-{$m[2]}-{$m[1]}");
+
+		} catch (Exception $e) {}
+
+		return FALSE;
+	}
+
+}
