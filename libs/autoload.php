1d493efdc7526a86c4b05c1d6f0386fab4907910
Petr Kessler <kesspess@gmail.com>
2013-04-20 16:43:00 +0200

    styling

--- libs/autoload.php
+++ libs/autoload.php
@@ -2,4 +2,4 @@
 
 require_once __DIR__ . '/Nette/loader.php';
 require_once __DIR__ . '/dibi/dibi.php';
-require_once __DIR__ . '/TwiGrid/twigrid.php';
+require_once __DIR__ . '/TwiGrid/src/twigrid.php';
