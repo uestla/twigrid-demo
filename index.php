877a5dd7b231ea51c1f7cf0b54e64730286fe3fd
Petr Kessler <kesspess@gmail.com>
2013-08-10 17:58:19 +0200

    routing updated

--- index.php
+++ index.php
@@ -10,5 +10,6 @@ $c->addConfig( __DIR__ . '/app/config.neon' );
 
 function id($a) { return $a; }
 $container = $c->createContainer();
+$container->router[] = new Nette\Application\Routers\Route('<action>', 'Example:default');
 $container->router[] = new Nette\Application\Routers\SimpleRouter('Example:default');
 $container->application->run();
